92d2e991ee64cb37ea3d227d09de3354e6c1f8ec
Andy McKay <amckay@mozilla.com>
2017-01-13 16:40:04 -0800

    add in CORS headers

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ var redis = require('redis');
 var db = redis.createClient(process.env.REDIS_URL);
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.configure(function(){
     app.use(express.bodyParser());
     app.use(express.static(__dirname + '/static'));
